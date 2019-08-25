7733f11e9f1c876311136b283ca9c6912617ac6d
pikachuim <pikachuim@qq.com>
2019-08-25 20:39:16 +0800

    pika-update

---
+++ EXIT/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	system(" taskkill /IM gam*");
+	system(" taskkill /IM JUM*");
+	system(" taskkill /IM GAM*");
+	system(" taskkill /IM ±º≈‹∞…ﬂ¥*");
+	system(" taskkill /IM gam*");
+	system(" taskkill /IM JUM*");
+	system(" taskkill /IM GAM*");
+	system(" taskkill /IM ±º≈‹∞…ﬂ¥*");
+	system(" pause");
+	return 0;
+}
